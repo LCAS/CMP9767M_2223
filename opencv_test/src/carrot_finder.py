ef8357075dd8ad5945d35297582b19725f6a05c8
Sean <15561571@students.lincoln.ac.uk>
2018-11-18 16:37:04 +0000

    Carrot Finder Dependancies streamlined, initialisations made for closing operation on image with 5x5 kernel

--- opencv_test/src/carrot_finder.py
+++ opencv_test/src/carrot_finder.py
@@ -3,9 +3,9 @@
 import rospy
 from cv2 import namedWindow, cvtColor, imshow
 from cv2 import destroyAllWindows, startWindowThread
-from cv2 import COLOR_BGR2GRAY, waitKey
+from cv2 import waitKey
 from cv2 import blur, Canny, threshold, THRESH_BINARY, split
-from numpy import median
+from numpy import ones, uint8
 from sensor_msgs.msg import Image
 from cv_bridge import CvBridge
 
@@ -14,8 +14,11 @@ class image_converter:
     def __init__(self):
 
         self.bridge = CvBridge()
+        #initialising subscriber
         self.image_sub = rospy.Subscriber("/thorvald_001/kinect2_camera/hd/image_color_rect",
                                           Image, self.callback)
+        #initialising kernel for closing operation                                   
+        self.kernel = ones((5,5), uint8)                                  
 
     def callback(self, data):
         namedWindow("Image window")
