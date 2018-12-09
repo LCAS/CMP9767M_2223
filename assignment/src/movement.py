5a99a412ed174a7f4fa8145cd146fa23b8497ccf
Sean <15561571@students.lincoln.ac.uk>
2018-12-09 19:40:12 +0000

    Planning

--- assignment/src/movement.py
+++ assignment/src/movement.py
@@ -7,6 +7,14 @@ from sensor_msgs.msg import LaserScan
 #MODULE ADAPTED FROM WORKSHOP 3
 ################
 
+#import tutorial scripts to launch fake_localisation and mapping
+
+#create subscriber to goals topic
+
+#add goals to local list and bounce off of that
+
+
+
 def scan_callback(data):
     # callback function for scan data
     pub = rospy.Publisher("/twist_mux/cmd_vel", Twist, queue_size = 1)
