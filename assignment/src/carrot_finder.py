4516f8e11c43c612939ccf59198ad2723f72c5e4
Sean <15561571@students.lincoln.ac.uk>
2018-12-11 16:05:15 +0000

    fake odometry successfully copied to local object, printing method implemented for debugging

--- assignment/src/carrot_finder.py
+++ assignment/src/carrot_finder.py
@@ -8,6 +8,7 @@ from cv2 import threshold, THRESH_BINARY, split, medianBlur, connectedComponents
 from cv2 import merge, cvtColor, COLOR_HSV2BGR
 from numpy import ones, uint8, max, ones_like
 from sensor_msgs.msg import Image
+from nav_msgs.msg import Odometry
 from cv_bridge import CvBridge
 
 class image_converter:
@@ -15,14 +16,27 @@ class image_converter:
     def __init__(self):
 
         self.bridge = CvBridge()
-        #initialising subscriber
+        #initialising subscribers
+        #initialising subscriber for camera data
         self.image_sub = rospy.Subscriber("/thorvald_001/kinect2_camera/hd/image_color_rect",
-                                          Image, self.callback)
-        #initialising kernel for closing operation                                   
-        self.kernel = ones((9,9), uint8)     
-        self.kernel_small = ones((5,5), uint8)                                   
+                                          Image, self.img_callback)
+        #subscriber for fake localisation odometry data
+        self.odom = rospy.Subscriber("/thorvald_001/odometry/base_raw", Odometry, self.odom_callback)
+        #subscriber for testing flag
 
-    def callback(self, data):
+        #initialising kernels for opening and closing operations                                   
+        self.kernel = ones((9,9), uint8)     
+        self.kernel_small = ones((5,5), uint8)
+
+        #initialising local odometry flags
+        self.posX = 0               #robot X pos
+        self.posY = 0               #robot Y pos
+        self.posZ = 0               #robot Z pos
+        self.angularX = 0           #robot angularX
+        self.angularY = 0           #robot angularY
+        self.angularZ = 0           #robot angularZ        
+
+    def img_callback(self, data):
         #OPENCV DISPLAY REMOVED FOR INCREASED PERFORMANCE
         #namedWindow("Image window")
 
@@ -35,10 +49,8 @@ class image_converter:
         #performing binary thresholding operation on green channel
         ret, thresh = threshold(g, 50, 255, THRESH_BINARY)
         
-        #consider replacing with opening, and increasing kernel size to further remove small objects/noise
+        #opening image to remove small objects/noise 
         open_result = morphologyEx(thresh, MORPH_OPEN, self.kernel)
-        #preforming median filter to remove noise
-        #median_result = medianBlur(thresh, 11)
         
         #performing closing operation to remove voids in remaining objects
         closed_img = morphologyEx(open_result, MORPH_CLOSE, self.kernel_small)
@@ -64,11 +76,47 @@ class image_converter:
         #OPENCV DISPLAY REMOVED FOR INCREASED PERFORMANCE#imshow("Image window", labeled_img)
         ##END DISPLAY METHODS        
         
-        #publishing image
+        #publishing image data to camera_objects topic
         output = self.bridge.cv2_to_imgmsg(labeled_img, encoding="rgb8")
         camera_objects.publish(output)
 
         waitKey(1)
+
+    def odom_callback(self, data):
+        #callback method for the odometry subscriber
+        #updates local flags with current odometry
+        self.posX = data.pose.pose.position.x                  #robot X pos
+        self.posY = data.pose.pose.position.y                  #robot Y pos
+        self.posZ = data.pose.pose.position.z                  #robot Z pos
+        self.angularX = data.pose.pose.orientation.x           #robot angularX
+        self.angularY = data.pose.pose.orientation.y           #robot angularY
+        self.angularZ = data.pose.pose.orientation.z           #robot angularZ       
+
+        self.print_local_odometry()
+
+    def print_local_odometry(self):
+        #outputs to terminal the current values in the local odometry    
+        print "pos x: " + str(self.posX) 
+        print "pos y: " + str(self.posY) 
+        print "pos z: " + str(self.posZ) 
+        print "orientation x: " + str(self.angularX) 
+        print "orientation y: " + str(self.angularY) 
+        print "orientation z: " + str(self.angularZ)  
+
+    def flag_callback(self, data):
+        #callback method for flag test
+        print "test"
+
+    #def getCoOrds(imageX, imageY):
+        #function returns spatial co-ords relative to robots frame given a camera value
+        #camera parameters
+    #    camera_angle = 45           #angle of camera
+    #    cam_height = 0.5            #height of camera from the ground
+    #    cam_displacement_x = 0.0    #displacement of the camera from robot center on x axis
+    #    cam_displacement_y = 0.0    #displacement of the camera from robot center on y axis
+
+    #def navigateTo(worldX, worldY):
+        #function sets a goal for the robot to navigate to 
 
 startWindowThread()
 rospy.init_node('image_converter')
