91b6a8e59bd2e876f7bafde7afdaedb7d94340df
Liam <liamgalloway@live.com>
2018-12-12 19:12:24 +0000

    new line logic

--- weeding/scripts/control.py
+++ weeding/scripts/control.py
@@ -2,7 +2,7 @@
 import rospy
 
 from std_msgs.msg import Bool
-from nav_msgs.msg import OccupancyGrid
+from nav_msgs.msg import OccupancyGrid, Odometry
 from simple_move_base import Go_To_Point
 
 class weed_control():
@@ -30,6 +30,7 @@ class weed_control():
 	self.unfin_path_sub = rospy.Subscriber('/unfinished_path', Bool, self.unfin_path_call)
 	self.spray = False
 	self.find_weeds = False
+	self.ends = False
 	self.map_places = []
 
     def create_map_places(self, data): #create searching points in the four corners of the square map
@@ -45,6 +46,14 @@ class weed_control():
     def unfin_path_call(self, data):
 	if data.data == True:
 		self.go_pub.publish(True)
+	elif data.data == False:
+		if self.ends == False:
+			pass#spin 180
+			self.go_pub.publish(True)
+			self.ends = True
+		else:
+			self.spray_pub.publish(True)
+			self.ends = False
 
     def found_weeds_call(self, data):
 	if data.data == True:
@@ -52,7 +61,6 @@ class weed_control():
 	elif data.data == False:
 		self.find_weeds_pub.publish(False)
 		self.spray_pub.publish(True)
-		self.spray = True
 	
     def spray_weeds_call(self, data):
 	if data.data == False:
@@ -72,7 +80,7 @@ class weed_control():
 	go_place.append(0)
 	success = move_base_commander.point(go_place, 120) #go_place = location for sprayer, 120 = time(s) until goal is rejected
 	if success == True:
-		pass 
+		self.go_pub.publish(True) 
 	
 rospy.init_node('weed_control', anonymous=True)
 wc = weed_control()
