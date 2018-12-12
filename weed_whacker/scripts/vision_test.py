83440961e5ebae9fa5764f8aaa1d900e08733fc4
Liam <liamgalloway@live.com>
2018-12-12 00:14:05 +0000

    row finding logic started

--- weed_whacker/scripts/vision_test.py
+++ weed_whacker/scripts/vision_test.py
@@ -13,6 +13,7 @@ from cv_bridge import CvBridge
 
 from sensor_msgs.msg import Image, CameraInfo
 from geometry_msgs.msg import PoseStamped
+from simple_move_base import Go_To_Point
 
 class image_converter:
     camera_model = None
@@ -22,10 +23,20 @@ class image_converter:
 
         self.bridge = CvBridge()
         self.image_sub = rospy.Subscriber('/thorvald_001/kinect2_camera/hd/image_color_rect',
-                                          Image, self.weeding)
+                                          Image, self.search)
+	self.camera_info_sub = rospy.Subscriber('/thorvald_001/kinect2_camera/hd/camera_info', 
+            				  CameraInfo, self.camera_info_callback)
 	self.tf_listener = tf.TransformListener()
 
-    def weeding(self, data): #weed finding in camera image
+    def camera_info_callback(self, data): #get camera info once
+	self.camera_model = image_geometry.PinholeCameraModel()
+        self.camera_model.fromCameraInfo(data)
+        self.camera_info_sub.unregister()
+
+    def search(self, data): #row finding in camera image
+
+	(trans, rot) = self.tf_listener.lookupTransform('map', 
+            'thorvald_001/kinect2_rgb_optical_frame', rospy.Time())
 
     	#set up hsv image from camera sub
     	cv_image = self.bridge.imgmsg_to_cv2(data, "bgr8")
@@ -43,33 +54,55 @@ class image_converter:
     	dilation = cv2.dilate(erode,kernel2,iterations = 10)
 
 	_, contours, hierarchy = cv2.findContours(dilation, cv2.RETR_LIST, cv2.CHAIN_APPROX_SIMPLE)
-	plants = []
-	for c in contours:
-		peri = cv2.arcLength(c, True)
-		area = cv2.contourArea(c)
-		if ((area / peri) >= 10.0):
-			plants.append(c)
-	for c in plants: #https://www.learnopencv.com/find-center-of-blob-centroid-using-opencv-cpp-python/
-		M = cv2.moments(c)
-		cX = 0
-		cY = 0
-		if M["m00"] != 0:
- 			cX = int(M["m10"] / M["m00"])
- 			cY = int(M["m01"] / M["m00"])
-		else:
- 			cX, cY = 0, 0
-		cv2.putText(dilation, "weed", (cX - 25, cY - 25),cv2.FONT_HERSHEY_SIMPLEX, 0.5, (100, 255, 255), 2)
-
 	
-	#resize for visualisation
-    	cv_image_s = cv2.resize(dilation, (0,0), fx=0.5, fy=0.5)
+	contours = sorted(contours, key=cv2.contourArea)
 
-    	cv2.imshow("Image test", cv_image_s)
-    	cv2.waitKey(1)
+	rows = []
+	for c in range(len(contours)):
+		x,y,w,h = cv2.boundingRect(contours[c])
+		x = x+w
+		y = y+(h/2)
+		ray = self.camera_model.projectPixelTo3dRay((x,y))
 
-cv2.startWindowThread()
+		p_point = PoseStamped()
+		p_point.header.frame_id = 'thorvald_001/kinect2_rgb_optical_frame'
+		p_point.pose.orientation.w = 1.0
+		p_point.pose.position.x = ray[0]
+		p_point.pose.position.y = ray[1]
+		p_point.pose.position.z = ray[2]
+		p_test = self.tf_listener.transformPose('map', p_point)
+
+		vector = [p_test.pose.position.x - trans[0],
+		  	  p_test.pose.position.y - trans[1],
+		  	  p_test.pose.position.z - trans[2]]
+		vector_n = []
+		for c in range(3):
+			vector_n.append(vector[c] / ((vector[0]*vector[0])+(vector[1]*vector[1])+(vector[2]*vector[2]))**.5)
+		change_mag = (0.0 - p_test.pose.position.z)/vector_n[2]
+		inter_vec = [p_test.pose.position.x + (vector_n[0]*change_mag),p_test.pose.position.y + (vector_n[1]*change_mag),0]
+		rows.append((inter_vec[0], inter_vec[1]))
+
+	row_dist = []
+	for r in range(len(rows)):
+		dist = (((rows[r][0]+trans[0])*(rows[r][0]+trans[0]))+((rows[r][1]+trans[1])*(rows[r][0]+trans[0])))**0.5
+		row_dist.append(dist)
+
+	if len(row_dist) != 0:
+		_,coord = min((row_dist[i],i) for i in xrange(len(row_dist)))
+		go_place = []
+		go_place.append(rows[coord][0])#x
+		go_place.append(rows[coord][1])#y
+		go_place.append(0)#ox
+		go_place.append(0)#oy
+		go_place.append(0)#oz
+		move_base_commander = Go_To_Point()
+		success = move_base_commander.point(go_place, 1) #go_place = location for sprayer, 20 = time(s) until goal is rejected
+
+		if success:
+			print "bam!"
+		else:
+			print "no bam :( "
+
 rospy.init_node('vis_test')
 ic = image_converter()
 rospy.spin()
-
-cv2.destroyAllWindows()
