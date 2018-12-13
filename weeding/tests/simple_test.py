9a56d4ccf4f0c894319f4a89135ef223a89d193c
Liam <liamgalloway@live.com>
2018-12-13 05:38:42 +0000

    xml

--- weeding/tests/simple_test.py
+++ weeding/tests/simple_test.py
@@ -7,8 +7,8 @@ import unittest
 class TestBareBones(unittest.TestCase):
     # test 1 == 1
     # only functions with 'test_'-prefix will be run!
-    def test_one_equals_one(self):
-        self.assertEquals(1, 1, "1!=1")
+    def test_two_equals_two(self):
+        self.assertEquals(2, 2, "2!=2")
 
 if __name__ == '__main__':
     #PKG = 'test_roslaunch'
