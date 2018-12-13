70e314fba3ce68bad449f0584078f4188f40708c
Liam <liamgalloway@live.com>
2018-12-13 06:03:44 +0000

    xml

--- weeding/tests/simple_test.py
+++ weeding/tests/simple_test.py
@@ -9,12 +9,35 @@ class TestBareBones(unittest.TestCase):
     # only functions with 'test_'-prefix will be run!
     def setUp(self):
 	self.x = 5
+	self.move_client = actionlib.SimpleActionClient("/move_base", MoveBaseAction) #start actionclient
+        self.move_client.wait_for_server(rospy.Duration(5)) #wait for server to start up
 
     def test_two_equals_two(self):
-	if self.x == 5:
-		self.assertTrue(True)
+	goal = MoveBaseGoal() # new MoveBaseGoal message
+	goal_pose = Pose() # new Pose message
+       
+        goal_pose.position.x = 1 # fill up pose pos x
+        goal_pose.position.y = 0 # fill up pose pos y
+        goal_pose.position.z = 0 # fill up pose pos z with 0
+        goal_pose.orientation.w = 1 # fill up pose ori w with 1
+        goal_pose.orientation.x = 0 # fill up pose ori x with 0
+        goal_pose.orientation.y = 0 # fill up pose ori y with 0
+        goal_pose.orientation.z = 0 # fill up pose ori z with 0
+       
+        goal.target_pose.header.stamp = rospy.Time.now() # set MoveBaseGoal time stamp to current time from rospy
+        goal.target_pose.header.frame_id = 'map' # set the frame to map
+        goal.target_pose.pose = goal_pose # set the pose to the above
+
+	self.move_client.send_goal(goal) # send new goal to actionlib server
+       
+        success = self.move_client.wait_for_result(rospy.Duration(60)) # wait up to 30s for result
+       
+        state = self.move_client.get_state() # get the state of the current goal
+
+        if success and state == GoalStatus.SUCCEEDED: # if success is true and state == succeeded
+        	self.assertTrue(True)
         else:
-		self.assertTrue(False, msg="5 not 5")
+		self.assertTrue(False, msg="timed out after %d seconds" % max_seconds)
 
 if __name__ == '__main__':
     #PKG = 'test_roslaunch'
