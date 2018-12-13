5f1f1997cf42737bb33f0a71e2f9d7df8ae2b034
Liam <liamgalloway@live.com>
2018-12-13 05:20:46 +0000

    xml

--- weeding/tests/weed_test.py
+++ weeding/tests/weed_test.py
@@ -1,16 +1,6 @@
 #!/usr/bin/env/python
 
 import unittest
-import rospy
-import actionlib
- 
-from move_base_msgs.msg import MoveBaseAction, MoveBaseGoal
-from actionlib_msgs.msg import *
-from geometry_msgs.msg import Pose
-from nav_msgs.msg import Odometry
-from geometry_msgs.msg import Twist
-from std_msgs.msg import Bool
-from math import sqrt
 
 class WeedTest(unittest.TestCase):
     def test_two_two_two(self):
