e16b439e00790ca4b6501099e26ca22abb13563a
Marc Hanheide <marc@hanheide.net>
2019-10-01 12:10:35 +0100

    more examples

---
+++ uol_cmp9767m_tutorial/scripts/add_two_ints_server.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+
+from uol_cmp9767m_tutorial.srv import AddTwoInts, AddTwoIntsResponse
+import rospy
+
+
+def handle_add_two_ints(req):
+    print "Returning [%s + %s = %s]" % (req.a, req.b, (req.a + req.b))
+    return AddTwoIntsResponse(req.a + req.b)
+
+
+def add_two_ints_server():
+    rospy.init_node('add_two_ints_server')
+    rospy.Service('add_two_ints', AddTwoInts, handle_add_two_ints)
+    print "Ready to add two ints."
+    rospy.spin()
+
+if __name__ == "__main__":
+    add_two_ints_server()
