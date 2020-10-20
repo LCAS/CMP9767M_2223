eee563dd2586ef4f4a8f9257bf917911f9779a7f
Marc Hanheide <marc@hanheide.net>
2020-10-20 16:29:39 +0100

    added documentation

--- uol_cmp9767m_tutorial/scripts/mover.py
+++ uol_cmp9767m_tutorial/scripts/mover.py
@@ -5,14 +5,28 @@ from geometry_msgs.msg import Twist
 from sensor_msgs.msg import LaserScan
 
 class Mover:
+    """
+    A very simple Roamer implementation for Thorvald.
+    It simply goes straight until any obstacle is within
+    3 m distance and then just simply turns left.
+    A purely reactive approach.
+    """
 
     def __init__(self):
+        """
+        On construction of the object, create a Subscriber
+        to listen to lasr scans and a Publisher to control
+        the robot
+        """
         self.publisher = rospy.Publisher(
             '/thorvald_001/teleop_joy/cmd_vel',
             Twist, queue_size=1)
         rospy.Subscriber("/thorvald_001/scan", LaserScan, self.callback)
 
     def callback(self, data):
+        """
+        Callback called any time a new laser scan becomes available
+        """
 
         rospy.loginfo(
             rospy.get_caller_id() + "I heard %s", data.header.seq)
