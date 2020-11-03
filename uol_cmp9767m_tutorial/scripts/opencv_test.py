adc1937304e771319bb554ce6e3abbde0fcb6793
Marc Hanheide <marc@hanheide.net>
2020-11-03 15:59:13 +0000

    added simple opencv code

---
+++ uol_cmp9767m_tutorial/scripts/opencv_test.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python
+
+import rospy
+from cv2 import namedWindow, cvtColor, imshow, inRange
+
+from cv2 import destroyAllWindows, startWindowThread
+from cv2 import COLOR_BGR2GRAY, waitKey
+from cv2 import blur, Canny
+from numpy import mean
+from sensor_msgs.msg import Image
+from cv_bridge import CvBridge
+
+
+class image_converter:
+
+    def __init__(self):
+
+        self.bridge = CvBridge()
+        self.image_sub = rospy.Subscriber("/thorvald_001/kinect2_camera/hd/image_color_rect",
+                                          Image, self.image_callback)
+        # self.image_sub = rospy.Subscriber(
+        #     "/camera/rgb/image_raw",
+        #     Image, self.callback)
+
+    def image_callback(self, data):
+        namedWindow("Image window")
+        namedWindow("masked")
+        namedWindow("canny")
+        cv_image = self.bridge.imgmsg_to_cv2(data, "bgr8")
+
+        mask = inRange(cv_image, (0, 100, 0), (255, 255, 255))
+        imshow("mask", mask)
+        gray_img = cvtColor(cv_image, COLOR_BGR2GRAY)
+        img3 = Canny(gray_img, 10, 200)
+        imshow("canny", img3)
+
+        imshow("Image window", cv_image)
+        waitKey(1)
+
+#startWindowThread()
+rospy.init_node('image_converter')
+ic = image_converter()
+rospy.spin()
+
+#destroyAllWindows()
