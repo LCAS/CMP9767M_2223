db20974bc7fb8637ee0870eaae8d47cada6fcb6a
Marc Hanheide <marc@hanheide.net>
2021-10-26 12:36:17 +0100

    simple opencv test

--- uol_cmp9767m_tutorial/scripts/opencv_test.py
+++ uol_cmp9767m_tutorial/scripts/opencv_test.py
@@ -5,7 +5,7 @@ from cv2 import namedWindow, cvtColor, imshow, inRange
 
 from cv2 import destroyAllWindows, startWindowThread
 from cv2 import COLOR_BGR2GRAY, waitKey
-from cv2 import blur, Canny
+from cv2 import blur, Canny, resize, INTER_CUBIC
 from numpy import mean
 from sensor_msgs.msg import Image
 from cv_bridge import CvBridge
@@ -16,7 +16,7 @@ class image_converter:
     def __init__(self):
 
         self.bridge = CvBridge()
-        self.image_sub = rospy.Subscriber("/thorvald_001/kinect2_camera/hd/image_color_rect",
+        self.image_sub = rospy.Subscriber("/thorvald_001/kinect2_front_camera/hd/image_color_rect",
                                           Image, self.image_callback)
         # self.image_sub = rospy.Subscriber(
         #     "/camera/rgb/image_raw",
@@ -27,9 +27,10 @@ class image_converter:
         namedWindow("masked")
         namedWindow("canny")
         cv_image = self.bridge.imgmsg_to_cv2(data, "bgr8")
+        cv_image = resize(cv_image, None, fx=0.2, fy=0.2, interpolation = INTER_CUBIC)
 
-        mask = inRange(cv_image, (0, 100, 0), (255, 255, 255))
-        imshow("mask", mask)
+        mask = inRange(cv_image, (0, 150, 150), (255, 255, 255))
+        imshow("masked", mask)
         gray_img = cvtColor(cv_image, COLOR_BGR2GRAY)
         img3 = Canny(gray_img, 10, 200)
         imshow("canny", img3)
