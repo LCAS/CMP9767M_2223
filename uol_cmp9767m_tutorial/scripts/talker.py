0faccec01a9b3a5a188882ded35f690283e1fa9d
Marc Hanheide <marc@hanheide.net>
2022-10-19 07:35:24 +0000

    simplified

--- uol_cmp9767m_tutorial/scripts/talker.py
+++ uol_cmp9767m_tutorial/scripts/talker.py
@@ -14,8 +14,5 @@ def talker():
         pub.publish(hello_str)
         rate.sleep()
 
-if __name__ == '__main__':
-    try:
-        talker()
-    except rospy.ROSInterruptException:
-        pass
+
+talker()
