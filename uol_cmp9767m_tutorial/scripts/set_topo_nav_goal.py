2262dafd8f3a8028a3a7fe59372588cd0e66a075
Marc Hanheide <marc@hanheide.net>
2022-11-28 22:13:19 +0000

    made work with toponav2

--- uol_cmp9767m_tutorial/scripts/set_topo_nav_goal.py
+++ uol_cmp9767m_tutorial/scripts/set_topo_nav_goal.py
@@ -8,7 +8,7 @@
 import rospy
 import actionlib
 
-from topological_navigation.msg import GotoNodeAction, GotoNodeGoal
+from topological_navigation_msgs.msg import GotoNodeAction, GotoNodeGoal
 
 if __name__ == '__main__':
     rospy.init_node('topological_navigation_client')
@@ -18,6 +18,7 @@ if __name__ == '__main__':
     # send first goal
     goal = GotoNodeGoal()
     goal.target = "WayPoint1"
+    rospy.loginfo("going to %s", goal.target)
     client.send_goal(goal)
     status = client.wait_for_result() # wait until the action is complete
     result = client.get_result()
@@ -25,7 +26,8 @@ if __name__ == '__main__':
     rospy.loginfo("result is %s", result)
 
     # send second goal
-    goal.target = "WayPoint5"
+    goal.target = "WayPoint6"
+    rospy.loginfo("going to %s", goal.target)
     # Fill in the goal here
     client.send_goal(goal)
     status = client.wait_for_result() # wait until the action is complete
